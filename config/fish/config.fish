a2894f7ffe43d2545bf55522b8aa145994f7eb37
Callum Doolin <callum@resolvedinstruments.com>
2023-09-01 09:26:54 -0600

    fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,10 +3,6 @@ if test -d ~/.venv/venv
     source ~/.venv/venv/bin/activate.fish
 end
 
-if type -q starship
-    starship init fish | source
-end
-
 if type -q batcat
     alias bat batcat
 end
@@ -20,4 +16,15 @@ if ps -C systemd > /dev/null
     alias tmux "systemd-run --scope --user tmux"
 end
 
+if test -d ~/.cargo/bin
+    fish_add_path --path ~/.cargo/bin
+end
+
+if test -d ~/bin
+    fish_add_path --path ~/bin
+end
+
+if type -q starship
+    starship init fish | source
+end
 
