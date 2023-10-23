258b3342f67893fafe74d80a28a50b0d6fbd7805
Callum Doolin <callum@resolvedinstruments.com>
2023-10-23 11:26:15 -0600

    enable copilot

--- config/nvim/lua/callum/plugins/init.lua
+++ config/nvim/lua/callum/plugins/init.lua
@@ -175,5 +175,15 @@ return {
     }
   },
   -- { 'karb94/neoscroll.nvim', opts = {} },
+  {
+    'github/copilot.vim',
+
+    config = function(_, _)
+      vim.keymap.set("n", "<leader>ce", ":Copilot enable<CR>", { desc = '[C]opilot [E]nable' })
+      vim.keymap.set("n", "<leader>cd", ":Copilot disable<CR>", { desc = '[C]opilot [D]isable' })
+      vim.keymap.set("n", "<leader>cs", ":Copilot status<CR>", { desc = '[C]opilot [S]tatus' })
+      vim.keymap.set("n", "<leader>cp", ":Copilot panel<CR>", { desc = '[C]opilot [P]anel' })
+    end
+  }
 
 }
