cccdf4d8c8070b2894e8c039231d1ec37c218e40
Callum Doolin <callum@resolvedinstruments.com>
2023-10-27 19:26:59 -0600

    update set_energy_performance

--- bin/set_energy_performance.sh
+++ bin/set_energy_performance.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+if [[ "$1" == "--help" ]]; then
+  echo "Available modes:"
+  cat /sys/devices/system/cpu/cpu0/cpufreq/energy_performance_available_preferences
+  exit 0
+fi
+
+MODE=${1:-power}
+echo setting power mode to $MODE
+
 for cpu_path in /sys/devices/system/cpu/cpu*/cpufreq/energy_performance_preference; do
-  echo "power" > "$cpu_path"
+  echo $MODE > "$cpu_path"
 done
 
