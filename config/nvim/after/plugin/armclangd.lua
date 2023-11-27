20a14c78e0f6e24413faa37f0b6d4d0044806653
Callum Doolin <callum@resolvedinstruments.com>
2023-11-27 16:59:49 -0700

    armclangd nvim function

---
+++ config/nvim/after/plugin/armclangd.lua
@@ -0,0 +1,20 @@
+get_gcc_location = function()
+	local gcc_location = vim.fn.system("which arm-none-eabi-gcc")
+	gcc_location = string.gsub(gcc_location, "\n", "")
+	return gcc_location
+end
+
+function ArmClangd()
+	vim.cmd("LspStop")
+	require("lspconfig").clangd.setup({
+		cmd = {
+			"/home/callum/.local/share/nvim/mason/bin/clangd",
+			-- "--query-driver=/home/callum/stm32cubeide/plugins/com.st.stm32cube.ide.mcu.externaltools.gnu-tools-for-stm32.11.3.rel1.linux64_1.1.1.202309131626/tools/bin/arm-none-eabi-gcc",
+			"--query-driver=" .. get_gcc_location(),
+		},
+	})
+
+	vim.cmd("LspStart")
+end
+
+vim.api.nvim_create_user_command("ArmClangd", ArmClangd, { desc = "Configure clangd for stm32 arm gcc" })
