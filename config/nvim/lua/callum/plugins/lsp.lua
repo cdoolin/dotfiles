232bc53f5470a9595e824509bdf0e894e9db80b3
Callum Doolin <callum@resolvedinstruments.com>
2024-02-24 20:44:55 -0700

    severity_limit deprecated

--- config/nvim/lua/callum/plugins/lsp.lua
+++ config/nvim/lua/callum/plugins/lsp.lua
@@ -111,7 +111,7 @@ local config_lsp = function(_, _)
   vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     vim.lsp.diagnostic.on_publish_diagnostics, {
       virtual_text = {
-        severity_limit = "Warning",
+        -- severity_limit = "Warning",
       },
     }
   )
