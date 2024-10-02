f47fa97c87fbc73ed02902eac78bce8f5091279e
Callum Doolin <callum@resolvedinstruments.com>
2024-10-02 08:12:41 -0600

    update conform syntax

--- config/nvim/lua/callum/plugins/format.lua
+++ config/nvim/lua/callum/plugins/format.lua
@@ -1,34 +1,34 @@
 return {
-  {
-    'stevearc/conform.nvim',
-    event = { "BufWritePre" },
-    cmd = { "ConformInfo" },
-    keys = {
-      {
-        -- Customize or remove this keymap to your liking
-        "<leader>cf",
-        function()
-          require("conform").format({ async = true, lsp_format = "fallback" })
-        end,
-        mode = "",
-        desc = "[C]ode [F]ormat",
-      },
-    },
-    -- lazy = true,
-    opts = {
-      formatters_by_ft = {
-        lua = { "stylua" },
-        python = { "black" },
-        javascript = { { "prettierd", "prettier" } },
-        javascriptreact = { { "prettierd", "prettier" } },
-        typescript = { { "prettierd", "prettier" } },
-        typescriptreact = { { "prettierd", "prettier" } },
-      },
-      format_on_save = {
-        timeout_ms = 1000,
-        lsp_fallback = true,
-      }
-    },
-    -- dependencies = { '' },
-  }
+	{
+		"stevearc/conform.nvim",
+		event = { "BufWritePre" },
+		cmd = { "ConformInfo" },
+		keys = {
+			{
+				-- Customize or remove this keymap to your liking
+				"<leader>cf",
+				function()
+					require("conform").format({ async = true, lsp_format = "fallback" })
+				end,
+				mode = "",
+				desc = "[C]ode [F]ormat",
+			},
+		},
+		-- lazy = true,
+		opts = {
+			formatters_by_ft = {
+				lua = { "stylua" },
+				python = { "black" },
+				javascript = { "prettierd", "prettier" },
+				javascriptreact = { "prettierd", "prettier" },
+				typescript = { "prettierd", "prettier" },
+				typescriptreact = { "prettierd", "prettier" },
+			},
+			format_on_save = {
+				timeout_ms = 1000,
+				lsp_fallback = true,
+			},
+		},
+		-- dependencies = { '' },
+	},
 }
